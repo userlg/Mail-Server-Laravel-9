0e3ed4f71c4fc302fe4e7960ba70ff1ce00cf489
userlg <guipeluis90@gmail.com>
2022-08-25 15:47:33 -0400

    Test database and mailable added

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Mail\NotifyMail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
 use Tests\TestCase;
 
 use App\Models\Message;
@@ -11,8 +13,6 @@ use App\Models\Message;
 class RoutesTest extends TestCase
 {
     use RefreshDatabase;
-    
-    use WithFaker;
     /**
      * A basic feature test example.
      *
@@ -27,15 +27,48 @@ class RoutesTest extends TestCase
         $response->assertOk();
     }
 
-    public function test_route_home_post(){
+    public function test_route_home_post()
+    {
+
 
         $this->withExceptionHandling();
-        
+
+        $this->withoutMiddleware();
+
+        $message = Message::factory()->make();
+
+        $data = [
+            'title' => $message->title,
+            'senderEmail' => $message->senderEmail,
+            'message' => $message->message
+        ];
+
+        $response = $this->post('/', $data);
+
+        $response->assertRedirect('http://localhost?email%20sent');
+
+
+        $this->assertCount(1, Message::all());
+    }
+
+    public function test_mail_server_database()
+    {
         $message = Message::factory()->create();
-        
-        $response = $this->post('/',array($message));
-         
-        $this->assertCount(1,Message::all());
-       // $response->assertCreated();
+
+        $this->assertDatabaseHas('messages', ['title' => $message->title]);
+    }
+
+    public function test_mailable_notifyMail()
+    {
+
+        $message = Message::factory()->create();
+
+        $mailable = new NotifyMail($message);
+
+        $mailable->assertSeeInHtml('From:');
+
+        $mailable->assertSeeInHtml($message->senderEmail);
+
+        $mailable->assertSeeInHtml($message->message);
     }
 }
